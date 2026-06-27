db6ef77211caf6d0268af13691a9a929d587913a
Justin Makeig <jmpublic@makeig.com>
2026-06-27 22:26:58 +0000

    Extracts icons into snippets (phew!)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,65 +19,15 @@
 					<div class="item">
 						{#if 'ref' in match && 'compact' !== mode}
 							<div class="ref">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="1em"
-									height="1em"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									class="icon icon-tabler icons-tabler-outline icon-tabler-building-store"
-									><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 21l18 0" /><path
-										d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4"
-									/><path d="M5 21l0 -10.15" /><path d="M19 21l0 -10.15" /><path
-										d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4"
-									/></svg
-								>
+								{@render icon_customer()}
 								{match.ref.name}
 							</div>
 						{/if}
 						<div>
 							{#if 'company' === match.type}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="1em"
-									height="1em"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									class="icon icon-tabler icons-tabler-outline icon-tabler-building-store"
-									><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 21l18 0" /><path
-										d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4"
-									/><path d="M5 21l0 -10.15" /><path d="M19 21l0 -10.15" /><path
-										d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4"
-									/></svg
-								>
+								{@render icon_customer()}
 							{:else if 'workload' === match.type}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="1em"
-									height="1em"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									class="icon icon-tabler icons-tabler-outline icon-tabler-truck-loading"
-									><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-										d="M2 3h1a2 2 0 0 1 2 2v10a2 2 0 0 0 2 2h15"
-									/><path
-										d="M9 9a3 3 0 0 1 3 -3h4a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-4a3 3 0 0 1 -3 -3l0 -2"
-									/><path d="M7 19a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path
-										d="M16 19a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"
-									/></svg
-								>
+								{@render icon_workload()}
 							{/if}
 							{match.name}
 						</div>
@@ -128,6 +78,48 @@
 		<div class="contents"><input id="vanilla" /></div>
 	</div>
 </form>
+
+{#snippet icon_customer(width = '1em', height = '1em')}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		{width}
+		{height}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="icon icon-tabler icons-tabler-outline icon-tabler-building-store"
+		><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 21l18 0" /><path
+			d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4"
+		/><path d="M5 21l0 -10.15" /><path d="M19 21l0 -10.15" /><path
+			d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4"
+		/></svg
+	>
+{/snippet}
+
+{#snippet icon_workload(width = '1em', height = '1em')}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		{width}
+		{height}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="icon icon-tabler icons-tabler-outline icon-tabler-truck-loading"
+		><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+			d="M2 3h1a2 2 0 0 1 2 2v10a2 2 0 0 0 2 2h15"
+		/><path
+			d="M9 9a3 3 0 0 1 3 -3h4a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-4a3 3 0 0 1 -3 -3l0 -2"
+		/><path d="M7 19a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path
+			d="M16 19a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"
+		/></svg
+	>
+{/snippet}
 
 <style>
 	form {
